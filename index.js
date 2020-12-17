bee5e68c5549e16e4071287d28586dd4a65d9e0a
Steven <stevenpmcfarlane@gmail.com>
2020-12-17 15:59:46 -0700

    Thursday afternoon

--- index.js
+++ index.js
@@ -42,7 +42,22 @@ class Airplane {
   */
   
  class Person {
-    
+    constructor(name, age){
+      this.name = name,
+      this.age = age,
+      this.stomach = []
+    }
+    eat(someFood){
+      if(this.stomach.length < 10){
+        this.stomach.push(someFood);
+      }
+    }
+    poop(){
+      return this.stomach = [];
+    }
+    toString(){
+      return `${this.name}, ${this.age}`;
+    }
   }
   
   /*
@@ -60,7 +75,30 @@ class Airplane {
   */
   
  class Car {
-    
+    constructor(model, milesPerGallon){
+      this.model = model,
+      this.milesPerGallon = milesPerGallon,
+      this.tank = 0,
+      this.odometer = 0
+    }
+    fill(gallons){
+      this.tank = this.tank + gallons
+    }
+    drive(distance){
+
+      let maxMiles = this.tank * this.milesPerGallon;
+      if(maxMiles < distance){
+        this.tank = 0;
+        this.odometer = this.odometer + maxMiles;
+        return `I ran out of fuel at ${maxMiles} miles!`;
+      } else{
+        this.tank = this.tank - distance/this.milesPerGallon;
+        this.odometer = this.odometer + distance;
+      }
+
+    }
+
+       
   }
   
   /*
@@ -75,9 +113,17 @@ class Airplane {
           + Speaking should return a phrase `Hello my name is {name}, I am from {location}`.
           + {name} and {location} of course come from the instance's own properties.
   */
- class Lambdasian {
-    
-  }
+ class Lambdasian{
+   constructor(attributes){
+    this.name = attributes.name,
+    this.age = attributes.age,
+    this.location = attributes.location
+   }
+   speak(){
+     return `Hello my name is ${this.name}, I am from ${this.location}`
+   }
+ }
+  
   
   /*
     TASK 4
@@ -93,7 +139,19 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
+ class Instructor extends Lambdasian{
+   constructor(obj){
+     super(obj);
+       this.specialty = obj.specialty,
+       this.favLanguage = obj.favLanguage,
+       this.catchPhrase = obj.catchPhrase
+     }
+     demo(subject){
+      return `Today we are learning about ${subject}`
+     }
+     grade(student, subject){
+       return `${student.name} receives a perfect score on ${subject}`
+     }
 
  }
   /*
@@ -111,7 +169,22 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
+ class Student extends Lambdasian{
+   constructor(stuObj){
+     super(stuObj);
+      this.previousBackground = stuObj.previousBackground,
+      this.className = stuObj.className,
+      this.favSubjects = stuObj.favSubjects
+   }
+   listSubjects(){
+    return `Loving ${this.favSubjects.toString()}!`
+   }
+   PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`
+   }
+   sprintChallenge(subject){
+     return `${this.name} has begun sprint challenge on ${subject}`
+   }
      
  }
   
@@ -128,8 +201,18 @@ class Airplane {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
-     
+ class ProjectManager extends Instructor{
+   constructor(projObj){
+     super(projObj);
+     this.gradClassName = projObj.gradClassName,
+     this.favInstructor = projObj.favInstructor
+   }
+   standUp(slack){
+     return`${this.name} announces to ${slack}, @channel standy times!`
+   }
+   debugsCode(student, subject){
+     return `${this.name} debugs ${student.name}'s code on ${subject}`
+   }
  }
   /*
     STRETCH PROBLEM (no tests!)
